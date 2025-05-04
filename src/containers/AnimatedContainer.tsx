46519da0d7f01fe3d0e57dfc604ea624cbb31165
abrown5421 <augustashtonbrown@gmail.com>
2025-05-04 18:19:44 -0400

    modding the animation container to go faster and only allow for one component on the screen at a time

--- src/containers/AnimatedContainer.tsx
+++ src/containers/AnimatedContainer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "animate.css";
 import { AnimatedContainerProps } from "../types/globalTypes";
 
@@ -9,11 +9,24 @@ const AnimatedContainer: React.FC<AnimatedContainerProps> = ({
   entry = "animate__fadeIn",
   exit = "animate__fadeOut",
 }) => {
+  const [shouldRender, setShouldRender] = useState(isEntering);
+
+  useEffect(() => {
+    if (isEntering) {
+      setShouldRender(true);
+    } else {
+      const timeout = setTimeout(() => setShouldRender(false), 500);
+      return () => clearTimeout(timeout);
+    }
+  }, [isEntering]);
+
+  if (!shouldRender) return null;
+
   return (
     <div
-      className={`animate__animated ${
+      className={`animate__faster animate__animated ${
         isEntering ? entry : exit
-      } ${className}`.trim()}
+      } ${className} animated-page`.trim()}
     >
       {children}
     </div>
