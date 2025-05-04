d306533a845ff0fbeebe83f597b58cf61af18c5d
abrown5421 <augustashtonbrown@gmail.com>
2025-05-04 19:34:25 -0400

    adding in a search form on the search page that allows the user to specify the artist, tour, venue, or year

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -3,10 +3,20 @@ import '../styles/pages/search-page.css';
 
 const SearchPage: React.FC = () => {
   return (
-    <div className='app-flex app-col app-jc-center app-ai-center app-bg-white search-page'>
-        search here
+    <div className="app-flex app-col app-jc-center app-ai-center app-bg-white search-page">
+      <div className="search-container">
+        <input type="text" className="search-input" placeholder="Artist" />
+        <input type="text" className="search-input" placeholder="Venue" />
+        <input type="text" className="search-input" placeholder="Tour" />
+        <input type="number" className="search-input" placeholder="Year" />
+        <button
+          className="app-button app-bg-primary app-font-black"
+        >
+          Search
+        </button>
+      </div>
     </div>
-  )
+  );
 };
 
 export default SearchPage;
